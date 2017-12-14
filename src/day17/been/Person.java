b84d86f0eaa64fd902d06263533272ff9ca89ff7
江南 <@DESKTOP-U5HVFSA>
2017-12-14 08:38:28 +0800

    工具类

--- src/day17/been/Person.java
+++ src/day17/been/Person.java
@@ -6,7 +6,7 @@ package day17.been;
  * 编写时间：12月10日
  */
 
-public class Person {
+public class Person /*extends Object*/ implements Comparable {
 
 	private String name;
 	private int age;
@@ -23,6 +23,34 @@ public class Person {
 		this.age = age;
 	}
 	
+	
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		
+//		System.out.println(this + "....hasCode");
+		return name.hashCode()+age*25;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		
+		if(this == obj)
+		{
+			return true;
+		}
+		if(!(obj instanceof Person))
+		{
+			throw new ClassCastException("类型不对");
+		}
+		
+//		System.out.println(this + "...equals.." + obj);
+		Person p = (Person) obj;
+		
+		return this.name.equals(p.name) && this.age == p.age;
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -34,6 +62,43 @@ public class Person {
 	}
 	public void setAge(int age) {
 		this.age = age;
+	}
+	
+	public String toString()
+	{
+		return name;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		
+		Person p = (Person) o;
+
+		//排序年龄
+		int temp = this.age - p.age;
+		return temp == 0 ? this.name.compareTo(p.name):temp;
+		
+		//按照姓名排序
+//		int temp = this.name.compareTo(p.name);
+//		return temp == 0?this.age-p.age:temp;
+		
+//		return this.age - p.age;
+		
+		//
+		
+		/*if(this.age > p.age)
+		{
+			return 1;
+		}
+		
+		if (this.age < p.age) {
+			return -1;
+		}
+		
+		else {
+			return this.name.compareTo(p.name);
+		}*/
 	}
 	
 
