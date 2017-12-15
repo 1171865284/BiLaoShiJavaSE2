a864081815f31b2d1e4d26858f6ef632f6e776b3
江南 <@DESKTOP-U5HVFSA>
2017-12-15 15:11:28 +0800

    泛型框架集合

---
+++ src/day18_2/GenericDemo2.java
@@ -0,0 +1,14 @@
+package day18_2;
+
+import java.util.TreeSet;
+
+public class GenericDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TreeSet ts = new TreeSet();
+		ts.add(ts);
+	}
+
+}
