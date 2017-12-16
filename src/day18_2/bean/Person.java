e2e2b42a523a6fae9ee39ee1ecc2feb6de336b23
江南 <@DESKTOP-U5HVFSA>
2017-12-16 23:13:19 +0800

    自定义工具类

--- src/day18_2/bean/Person.java
+++ src/day18_2/bean/Person.java
@@ -1,22 +1,80 @@
 package day18_2.bean;
 
-public class Person {
+import javax.management.RuntimeErrorException;
+
+public class Person implements Comparable<Person> {
 
 	private String name;
 	private int age;
 	
-	//构造函数
+	
+	public Person() {
+		super();
+		
+	}
+	
+	
+	
+	
 	public Person(String name, int age) {
 		super();
 		this.name = name;
 		this.age = age;
 	}
-	
-	//空参构造函数
-	public Person() {
-		super();
-		// TODO Auto-generated constructor stub
+
+
+	public int compareTo(Person p){
+		
+//		Person p = (Person)obj;
+		int temp = this.age - p.age;
+		return temp==0?this.name.compareTo(p.name):temp;
 	}
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+
+
+
 	public String getName() {
 		return name;
 	}
@@ -29,5 +87,17 @@ public class Person {
 	public void setAge(int age) {
 		this.age = age;
 	}
+
+
+
+
+	@Override
+	public String toString() {
+		
+		return "Person:"+getName()+":"+getAge();
+	}
+	
+	
+	
 	
 }
---
+++ src/day18_2/bean/Student.java
@@ -0,0 +1,25 @@
+package day18_2.bean;
+
+public class Student extends Person {
+
+	public Student() {
+		super();
+		
+	}
+
+	public Student(String name, int age) {
+		super(name, age);
+		
+	}
+
+	@Override
+	public String toString() {
+		
+		return "Student:"+getName()+":"+getAge();
+	}
+	
+	
+
+	
+}
+
