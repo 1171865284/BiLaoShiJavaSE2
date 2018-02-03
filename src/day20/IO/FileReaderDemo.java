ef367af8176183d910be3c5ade5c5aaa9cdbf237
Cyan-King <Cyan-King@DESKTOP-S5D98PM>
2018-02-03 14:10:56 +0800

    IO--读取数据一次读一个的方法案例

--- src/day20/IO/FileReaderDemo.java
+++ src/day20/IO/FileReaderDemo.java
@@ -25,25 +25,20 @@ public class FileReaderDemo {
 		 * 
 		 * 用一个读取流关联一个已存在文件
 		 */
+		//这个方法是一次读一个
+		//首先我们使用FileReader类来读取数据
+		FileReader fr = new FileReader("demo.txt");//读取我们需要读取的文件
 		
-		FileReader fr = new FileReader("demo.txt");
-		
+		//因为读取的数据都是二进制数字，所以我们设置一个二进制数字做为变量
 		int ch = 0;
-		
-		while((ch = fr.read()) != -1){
+		//再使用while循环来读取demo.txt中的数据
+		while((ch=fr.read())!= -1){
+			
+			//进行while循环一直读下去，一直读到-1为止
 			System.out.println((char)ch);
 		}
 		
-		/*
-		//用Reader中的read方法读取字符
-		int ch = fr.read();
-		System.out.println((char)ch);
-		int ch1 = fr.read();
-		System.out.println(ch1);
-		int ch2 = fr.read();
-		System.out.println(ch2);
-		*/
-		
+		//关闭数据
 		fr.close();
 
 	}
