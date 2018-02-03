f8c74e35ee1b29d3622a599b558349219f9b3b70
Cyan-King <Cyan-King@DESKTOP-S5D98PM>
2018-02-03 14:35:06 +0800

    IO——IOException，IO流异常

--- src/day20/IO/IOExceptionDemo.java
+++ src/day20/IO/IOExceptionDemo.java
@@ -11,26 +11,45 @@ import java.io.IOException;
 
 public class IOExceptionDemo {
 
-	private static final String LINE_SEPARATOR = System.getProperty("line.sperarator");
+	private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
+
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		/*
+		 * FileWriter fw = null; try { fw = new FileWriter("k:\\demo.txt",
+		 * true);
+		 * 
+		 * fw.write("abcde" + LINE_SEPARATOR + "hahaha");
+		 * 
+		 * 
+		 * } catch (IOException e) { // TODO: handle exception
+		 * System.out.println(e.toString()); }finally { if(fw != null) try {
+		 * fw.close(); } catch (IOException e) { throw new
+		 * RuntimeException("关闭失败"); } }
+		 */
 		FileWriter fw = null;
 		try {
-			 fw = new FileWriter("k:\\demo.txt", true);
+			fw = new FileWriter("demo.txt");
 
-			fw.write("abcde" + LINE_SEPARATOR + "hahaha");
+			fw.write("abc" + LINE_SEPARATOR + "cccc");
 
-			
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e.toString());
-		}finally {
-			if(fw != null)
-				try {
-					fw.close();
-				} catch (IOException e) {
-					throw new RuntimeException("关闭失败");
-					}
+		}
+		finally {
+			try {
+				fw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
