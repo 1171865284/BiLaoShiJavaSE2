6c0c41f182751dff0d9f89b2b6fb5f1b26792bd5
Cyan-King <Cyan-King@192.168.7.4>
2018-03-15 22:01:50 +0800

    网络编程---UDP---接收端和发送端

---
+++ src/day26/UDPSendDemo.java
@@ -0,0 +1,46 @@
+package day26;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+/*
+ * 程序功能：网络编程的UDP传输
+ * 程序员：魏国平
+ * 编写时间：3月15日
+ */
+
+/*
+ * 创建UDP传输的发送端
+ * 思路：
+ * 1.创建udp的socket服务
+ * 2.将要发送的数据封装到数据包中。
+ * 3.通过udp的socket服务将数据包发送出去
+ * 4.关闭socket服务
+ */
+public class UDPSendDemo {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		//创建UDP的socket服务，使用DatagramSocket类
+		DatagramSocket ds = new DatagramSocket();
+		
+		//将要发送的数据封装起来
+		String str = "这就是我们要发送的数据啦！";
+		
+		//我们使用Datagrampacket封装到数据保包
+		byte [] buf = new byte[1024];
+		DatagramPacket dp = new DatagramPacket(buf, buf.length, InetAddress.getByName("192.168.7.4"), 1000);
+		
+		//3.通过UDP的socket服务将数据包发送出去
+		ds.send(dp);
+		
+		//4.将sockt服务关闭
+		ds.close();
+	}
+
+}
