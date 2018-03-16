e0b38d4f4a129e74428649c5071a47f9abf201ca
Cyan-King <Cyan-King@192.168.7.8>
2018-03-16 15:53:35 +0800

    网络编程----UDP--Chat

---
+++ src/day26/net/chat/Chat.java
@@ -0,0 +1,23 @@
+package day26.net.chat;
+
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Chat {
+
+	public static void main(String[] args) throws SocketException {
+		// TODO Auto-generated method stub
+
+		
+		DatagramSocket send = new DatagramSocket();
+		DatagramSocket rece = new DatagramSocket(10000);
+		
+		Send s = new Send(send);
+		Rece r = new Rece(rece);
+		
+		new Thread(s).start();
+		new Thread(r).start();
+		
+	}
+
+}
